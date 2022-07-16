8876cf1d3ba88c5846f92903de9537e2e82bc4e6
Charan A A <104826220+mariswarycharan@users.noreply.github.com>
2022-07-16 14:52:36 +0530

    Add files via upload

---
+++ chess_board.py
@@ -0,0 +1,169 @@
+import tkinter
+
+
+from tkinter import *
+root = Tk()
+
+summa = Label(root,padx=130,pady=30)
+summa.grid(row=1,column=1)
+
+
+
+label = Label(root, text="  CHESS BOARD   ",font=("ALGERIAN",80) ,foreground="white",background="black",padx = 100)
+label.grid(row=1,column=2,columnspan=8)
+
+b1 = Button(root,padx=60,pady=30, background="black")
+b1.grid(row=2,column=2)
+b2 = Button(root,padx=60,pady=30, background="white")
+b2.grid(row=2,column=3)
+b3 = Button(root,padx=60,pady=30, background="black")
+b3.grid(row=2,column=4)
+b4 = Button(root,padx=60,pady=30, background="white")
+b4.grid(row=2,column=5)
+b5 = Button(root,padx=60,pady=30, background="black")
+b5.grid(row=2,column=6)
+b6 = Button(root,padx=60,pady=30, background="white")
+b6.grid(row=2,column=7)
+b7 = Button(root,padx=60,pady=30, background="black")
+b7.grid(row=2,column=8)
+b8 = Button(root,padx=60,pady=30, background="white")
+b8.grid(row=2,column=9)
+
+
+b9 = Button(root,padx=60,pady=30, background="white")
+b9.grid(row=3,column=2)
+b10 = Button(root,padx=60,pady=30, background="black")
+b10.grid(row=3,column=3)
+b11 = Button(root,padx=60,pady=30, background="white")
+b11.grid(row=3,column=4)
+b12 = Button(root,padx=60,pady=30, background="black")
+b12.grid(row=3,column=5)
+b13 = Button(root,padx=60,pady=30, background="white")
+b13.grid(row=3,column=6)
+b14 = Button(root,padx=60,pady=30, background="black")
+b14.grid(row=3,column=7)
+b15 = Button(root,padx=60,pady=30, background="white")
+b15.grid(row=3,column=8)
+b16 = Button(root,padx=60,pady=30, background="black")
+b16.grid(row=3,column=9)
+
+
+b17 = Button(root,padx=60,pady=30, background="black")
+b17.grid(row=4,column=2)
+b18 = Button(root,padx=60,pady=30, background="white")
+b18.grid(row=4,column=3)
+b19 = Button(root,padx=60,pady=30, background="black")
+b19.grid(row=4,column=4)
+b20 = Button(root,padx=60,pady=30, background="white")
+b20.grid(row=4,column=5)
+b21 = Button(root,padx=60,pady=30, background="black")
+b21.grid(row=4,column=6)
+b22 = Button(root,padx=60,pady=30, background="white")
+b22.grid(row=4,column=7)
+b23 = Button(root,padx=60,pady=30, background="black")
+b23.grid(row=4,column=8)
+b24 = Button(root,padx=60,pady=30, background="white")
+b24.grid(row=4,column=9)
+
+
+b9 = Button(root,padx=60,pady=30, background="white")
+b9.grid(row=5,column=2)
+b10 = Button(root,padx=60,pady=30, background="black")
+b10.grid(row=5,column=3)
+b11 = Button(root,padx=60,pady=30, background="white")
+b11.grid(row=5,column=4)
+b12 = Button(root,padx=60,pady=30, background="black")
+b12.grid(row=5,column=5)
+b13 = Button(root,padx=60,pady=30, background="white")
+b13.grid(row=5,column=6)
+b14 = Button(root,padx=60,pady=30, background="black")
+b14.grid(row=5,column=7)
+b15 = Button(root,padx=60,pady=30, background="white")
+b15.grid(row=5,column=8)
+b16 = Button(root,padx=60,pady=30, background="black")
+b16.grid(row=5,column=9)
+
+
+b17 = Button(root,padx=60,pady=30, background="black")
+b17.grid(row=6,column=2)
+b18 = Button(root,padx=60,pady=30, background="white")
+b18.grid(row=6,column=3)
+b19 = Button(root,padx=60,pady=30, background="black")
+b19.grid(row=6,column=4)
+b20 = Button(root,padx=60,pady=30, background="white")
+b20.grid(row=6,column=5)
+b21 = Button(root,padx=60,pady=30, background="black")
+b21.grid(row=6,column=6)
+b22 = Button(root,padx=60,pady=30, background="white")
+b22.grid(row=6,column=7)
+b23 = Button(root,padx=60,pady=30, background="black")
+b23.grid(row=6,column=8)
+b24 = Button(root,padx=60,pady=30, background="white")
+b24.grid(row=6,column=9)
+
+
+
+b9 = Button(root,padx=60,pady=30, background="white")
+b9.grid(row=7,column=2)
+b10 = Button(root,padx=60,pady=30, background="black")
+b10.grid(row=7,column=3)
+b11 = Button(root,padx=60,pady=30, background="white")
+b11.grid(row=7,column=4)
+b12 = Button(root,padx=60,pady=30, background="black")
+b12.grid(row=7,column=5)
+b13 = Button(root,padx=60,pady=30, background="white")
+b13.grid(row=7,column=6)
+b14 = Button(root,padx=60,pady=30, background="black")
+b14.grid(row=7,column=7)
+b15 = Button(root,padx=60,pady=30, background="white")
+b15.grid(row=7,column=8)
+b16 = Button(root,padx=60,pady=30, background="black")
+b16.grid(row=7,column=9)
+
+
+
+b17 = Button(root,padx=60,pady=30, background="black")
+b17.grid(row=8,column=2)
+b18 = Button(root,padx=60,pady=30, background="white")
+b18.grid(row=8,column=3)
+b19 = Button(root,padx=60,pady=30, background="black")
+b19.grid(row=8,column=4)
+b20 = Button(root,padx=60,pady=30, background="white")
+b20.grid(row=8,column=5)
+b21 = Button(root,padx=60,pady=30, background="black")
+b21.grid(row=8,column=6)
+b22 = Button(root,padx=60,pady=30, background="white")
+b22.grid(row=8,column=7)
+b23 = Button(root,padx=60,pady=30, background="black")
+b23.grid(row=8,column=8)
+b24 = Button(root,padx=60,pady=30, background="white")
+b24.grid(row=8,column=9)
+
+
+b9 = Button(root,padx=60,pady=30, background="white")
+b9.grid(row=9,column=2)
+b10 = Button(root,padx=60,pady=30, background="black")
+b10.grid(row=9,column=3)
+b11 = Button(root,padx=60,pady=30, background="white")
+b11.grid(row=9,column=4)
+b12 = Button(root,padx=60,pady=30, background="black")
+b12.grid(row=9,column=5)
+b13 = Button(root,padx=60,pady=30, background="white")
+b13.grid(row=9,column=6)
+b14 = Button(root,padx=60,pady=30, background="black")
+b14.grid(row=9,column=7)
+b15 = Button(root,padx=60,pady=30, background="white")
+b15.grid(row=9,column=8)
+b16 = Button(root,padx=60,pady=30, background="black")
+b16.grid(row=9,column=9)
+
+
+
+
+
+
+
+ 
+ 
+root.mainloop()
+ 
